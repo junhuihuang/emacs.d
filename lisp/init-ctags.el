6e7bab76ddec6a9daf476bd544fed2c727470aa3
leslie <leslie@huangjunhui-MacBook-Pro.local>
2016-05-02 13:49:30 +0800

    add ctags autoload for specific TAGS

--- lisp/init-ctags.el
+++ lisp/init-ctags.el
@@ -16,4 +16,10 @@
 (autoload 'etags-select-find-tag "etags-select" "" t nil)
 ;; }}
 
+;; stevens
+;; http://blog.binchen.org/posts/how-to-use-ctags-in-emacs-effectively-3.html
+(if (file-exists-p (expand-file-name "~/bemetoy_svn/BemeNew/poppy/BemetoyServer"))
+(setq tags-table-list '("~/bemetoy_svn/BemeNew/poppy/BemetoyServer/common/TAGS" "~/bemetoy_svn/BemeNew/poppy/common/TAGS"))
+(setq tags-table-list '("~/Dropbox/project/bemetoy_svn/BemeNew/poppy/BemetoyServer/common/TAGS" "~/Dropbox/project/bemetoy_svn/BemeNew/poppy/common/TAGS")))
+
 (provide 'init-ctags)
